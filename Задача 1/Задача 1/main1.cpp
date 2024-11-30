1c70ba0f8a4afc4bdf59c76ed80886999e18401b
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-30 18:52:26 +0300

    Задача 1

---
+++ Задача 1/Задача 1/main1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void swap_a_b(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main() {
+	int a = 10;
+	int b = 20;
+	int* ap = &a;
+	int* bp = &b;
+	swap_a_b(ap, bp);
+	cout << a << b;
+}
